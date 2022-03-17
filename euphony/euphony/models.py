62686672b3c2ad6c12330619e57951e5270b0166
bellascar <scarav14@students.rowan.edu>
2022-03-16 20:05:47 -0400

    Playlist table

--- euphony/euphony/models.py
+++ euphony/euphony/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Song(models.Model):
 	artist = models.TextField()
 	duration_ms = models.FloatField()
 	explicit = models.FloatField()
-    	id = models.TextField(primary_key=True)
-    	name = models.TextField()
-    	release_date = models.IntegerField()
-    	year = models.IntegerField()
+	id = models.TextField(primary_key=True)
+	name = models.TextField()
+	release_date = models.IntegerField()
+	year = models.IntegerField()
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist_name = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Song)
+    
