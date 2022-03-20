5a9405cf9dc4735a87d23a1b47c093066a28bae7
yellowjacket77sc <yellowjacket77sc@DESKTOP-647AP2C.localdomain>
2022-03-19 23:46:40 -0400

    album table

--- euphony/euphony/models.py
+++ euphony/euphony/models.py
@@ -8,3 +8,11 @@ class Song(models.Model):
     	name = models.TextField()
     	release_date = models.IntegerField()
     	year = models.IntegerField()
+
+class Album(models.Model):
+    name = models.TextField()
+    artist = models.TextField()
+    songs = mode;s.ManyToManyField(Song)
+    year = models.IntegerField()
+    duration_ms = models.FloatField()
+    id = models.TextField(primary_key=True)
