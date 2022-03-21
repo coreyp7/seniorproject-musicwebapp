fd412e7403ba83d467448ed3f41b8b1e2203eb94
coreyp7 <79476320+coreyp7@users.noreply.github.com>
2022-03-21 12:04:52 -0400

    Project properly migrates all databases

---
+++ euphony/databases/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Album(models.Model):
+    artist = models.TextField()  # Just artist name, not an ID or anything
+    duration = models.FloatField()  # Length of album
+    explicit = models.FloatField()
+    id = models.TextField(primary_key=True)  # Spotify ID of album
+    name = models.TextField()
+    release_date = models.TextField()
+
+
+class Song(models.Model):
+    id = models.TextField(primary_key=True)  # Spotify ID of song
+    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.TextField()
+    artist = models.TextField()
+    duration_ms = models.FloatField()
+    explicit = models.FloatField()
+    release_date = models.TextField()
+
+
+class Playlist(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(max_length=200)
+    songs = models.ManyToManyField(Song)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# Section dedicated towards each rating table for songs/albums/playlists.
+
+
+class Song_rating(models.Model):
+    id = models.TextField(primary_key=True)  # unique ID of this rating
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None
+    )  # ID of user who left this rating
+    song_id = models.ForeignKey(
+        Song, on_delete=models.CASCADE, default=None
+    )  # ID of song which this rating corresponds to
+    rating_type = models.BooleanField()  # True = Upvote, False = Downvote
+    date = (
+        models.TextField()
+    )  # date that the rating was given. Helpful for getting most recent feed
+
+
+class Album_rating(models.Model):
+    id = models.TextField(primary_key=True)  # unique ID of this rating
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None
+    )  # ID of user who left this rating
+    album_id = models.ForeignKey(
+        Album, on_delete=models.CASCADE, default=None
+    )  # ID of album which this rating corresponds to
+    rating_type = models.BooleanField()  # True = Upvote, False = Downvote
+    date = (
+        models.TextField()
+    )  # date that the rating was given. Helpful for getting most recent feed
+
+
+class Playlist_rating(models.Model):
+    id = models.TextField(primary_key=True)  # unique ID of this rating
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None
+    )  # ID of user who left this rating
+    playlist_id = models.ForeignKey(
+        Playlist, on_delete=models.CASCADE, default=None
+    )  # ID of playlist which this rating corresponds to
+    rating_type = models.BooleanField()  # True = Upvote, False = Downvote
+    date = (
+        models.TextField()
+    )  # date that the rating was given. Helpful for getting most recent feed
+
--- euphony/euphony/settings.py
+++ euphony/euphony/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-9*qfmv5xf(w2abn#b%+^!(k@z-wdzk!g!69d7r!!qu4#-vkv'
+SECRET_KEY = "django-insecure-&3qoqxs9adqy3^ia8gym)$g$=rz1xe=6ev6)*8o24fe%!c=pqi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,67 +31,67 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'account_link',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_comments_xtd',
-    'django_comments',
-    'django-friendship'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",  # needed for comments for some reason
+    "account_link",
+    "databases",
+    "django_comments_xtd",
+    "django_comments",
+    "friendship",
 ]
 
-#SITE_ID = 1
-
 # comments settings
-COMMENTS_APP = 'django_comments_xtd'
+COMMENTS_APP = "django_comments_xtd"
 
 COMMENTS_XTD_CONFIRM_EMAIL = False
 
 COMMENTS_XTD_MAX_THREAD_LEVEL = 3
 
-COMMENTS_XTD_LIST_ORDER = ('-thread_id', 'order') 
+COMMENTS_XTD_LIST_ORDER = ("-thread_id", "order")
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'euphony.urls'
+ROOT_URLCONF = "euphony.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'euphony.wsgi.application'
+WSGI_APPLICATION = "euphony.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -101,26 +101,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -130,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
