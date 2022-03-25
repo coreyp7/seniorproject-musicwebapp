920c7b1df43d79d59b2ede72a4a046914165f18e
Corey Pierce <coreypierce11@gmail.com>
2022-03-25 17:56:57 -0400

    Basic spotipy implementation, obtaining json from query

--- euphony/urls.py
+++ euphony/urls.py
@@ -4,5 +4,6 @@ from . import views  # get views.py from current directory
 urlpatterns = [
     path("", views.home, name="home"),
     path("account_link", views.link_account, name="link_account"),
-    path("search_song", views.search_song, name="search_song")
+    path("search_song", views.search_song, name="search_song"),
+    path("search_song_results", views.search_song_results, name="search_song_results")
 ]
--- euphony/views.py
+++ euphony/views.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST, require_GET
 
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from numpy.random import default_rng
+import json
 
 from .cashe_handler import DatabaseTokenHandler
 from .forms import SongForm
+
+scope = "user-library-read"
+#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 def home(request):
     return render(request, "home.html", {})
@@ -38,19 +44,42 @@ def link_account(request):
 
     return redirect("/")
 
+# Search song page that is blank, what is initially shown to user.
 @require_GET
 def search_song(request):
     form = SongForm()
     return render(request, "search_song.html", {"form": form})
 
+# Search song page that is displaying any results that the user requested 
+# on the search_song page.
 @require_POST
 def search_song_results(request):
     form = SongForm(request.POST)
 
     if form.is_valid():
         # find song list from spotipy and return the list in the form of a parameter in our render
+        track_query = "track:"+form.cleaned_data["song_name"]
+        songs_json = sp.search(track_query) # json with song information
+        #songs = json.load(songs_json)
+        #tracks = songs_json["tracks"]
+        print(json.dumps(songs_json, indent=4, sort_keys=True))
+
+        # Now we need to just take the information we need in the json and create a new dictionary.
+        # The things we need are:
+        # I THINK ALL OF THESE ARE IN "items".
+        # "id" -> the spotify specific id for this song
+        # "name" -> the name of the song
+        # "track_number" -> the placement of this song in its album
+        # "href" -> not sure, seems useful
+        # "explicit" -> true/false
+        # "artists":"name" -> artist name
+        # "album":"name" -> album name
+        # "album":"release_date" -> album release date
+        # "album":"id" -> album id
+        # There is also a "href" in every track object that is a link to the query.
+        # Whatever that means.
         pass
     else:
         print("unsuccessful :(")
 
-    return render(request, "search_song.html", {"form_info": form})
+    return render(request, "search_song.html", {"form_info": form, "songs": songs_json})
