555ea531798b96c318c3e115e95a7f26c2ddae33
yellowjacket77sc <90854627+yellowjacket77sc@users.noreply.github.com>
2022-04-30 11:08:41 -0400

    added the textfield to change bio

--- euphony/forms.py
+++ euphony/forms.py
@@ -62,6 +62,6 @@ class ProfileForm(forms.Form):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_pic']
+        fields = ['profile_pic', 'bio']
 
 
