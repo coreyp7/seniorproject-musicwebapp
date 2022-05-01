4c7e03408811e0b9c3e628270a05d4292486a42f
HyKris <hykris@DESKTOP-S04ALU2.localdomain>
2022-04-30 23:34:12 -0400

    rip

--- euphony_app/settings.py
+++ euphony_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
